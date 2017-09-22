a064c9e3f13deecbd65498e36ea0a647e3b9a665
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-22 18:05:25 +0530

    formatted and beautified code on the user side

--- home.php
+++ home.php
@@ -1,42 +1,39 @@
 <html>
-	     <?php include 'home-menu.php'; ?>
-       <?php include 'user-side-menu.php' ; ?>
+<?php include 'home-menu.php'; ?>
+    <?php include 'user-side-menu.php' ; ?>
 
-       <div class="col-md-9">
+        <div class="col-md-9">
             <!-- Website Overview -->
             <div class="panel panel-default">
-              <div class="panel-heading main-color-bg">
-                <h3 class="panel-title">Instructions</h3>
-              </div>
-              <div class="panel-body">
-              <p>1. Once registered on the portal you can apply for individual Courses given in the options </p>
-              <p>2. You'll have to submit your educational details in your profile in the 'Profile' section. </p>
-              <p>  </p>
-              <p> </p>
-<!--                <div class="col-md-4">
+                <div class="panel-heading main-color-bg">
+                    <h3 class="panel-title">Instructions</h3>
+                </div>
+                <div class="panel-body">
+                    <p>1. Once registered on the portal you can apply for individual Courses given in the options </p>
+                    <p>2. You'll have to submit your educational details in your profile in the 'Profile' section. </p>
+                    <p> </p>
+                    <p> </p>
+                    <!--                <div class="col-md-4">
                   <div class="well dash-box">
                     <h2><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 203</h2>
                     <h4>Users</h4>
                   </div>
 -->
-              </div>
+                </div>
             </div>
-          </div>
+        </div>
 
-
-
-          
         <!-- Payment Instructions -->
         <div class="col-md-9">
             <div class="panel panel-default">
-              <div class="panel-heading main-color-bg">
-                <h3 class="panel-title">Payment Instructions</h3>
-              </div>
-              <div class="panel-body">
-              <p> 1. Form Fees : Rs. 1000/- Nonrefundable ( payment mode : online by master/visa card OR offline at university ) </p>
-              <p> 2. Do not refresh (F5)form while filling data. </p>
-              </div>
+                <div class="panel-heading main-color-bg">
+                    <h3 class="panel-title">Payment Instructions</h3>
+                </div>
+                <div class="panel-body">
+                    <p> 1. Form Fees : Rs. 1000/- Nonrefundable ( payment mode : online by master/visa card OR offline at university ) </p>
+                    <p> 2. Do not refresh (F5)form while filling data. </p>
+                </div>
             </div>
-        </div> 
+        </div>
 
 </html>
--- user-side-menu.php
+++ user-side-menu.php
@@ -1,17 +1,14 @@
-
-    <section id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3">
-            <div class="list-group">
-              <a href="home.php" class="list-group-item active main-color-bg">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Sidemenu
-              </a>
-              <a href="home.php" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Home <span class="badge"></span></a>
-              <a href="profile.php" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Profile<span class="badge"></span></a>
-              <a href="apply.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Apply<span class="badge"></span></a>
-              <a href="user-test.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Entrance Test <span class="badge"></span></a>
-            </div>
-          </div> 
-
-		  
+<section id="main">
+<div class="container">
+<div class="row">
+<div class="col-md-3">
+   <div class="list-group">
+      <a href="home.php" class="list-group-item active main-color-bg">
+      <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Sidemenu
+      </a>
+      <a href="home.php" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Home <span class="badge"></span></a>
+      <a href="profile.php" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Profile<span class="badge"></span></a>
+      <a href="apply.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Apply<span class="badge"></span></a>
+      <a href="user-test.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Entrance Test <span class="badge"></span></a>
+   </div>
+</div>
