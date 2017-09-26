1996e517353de90f37d01aca1a53e15097c7c561
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-26 19:26:57 +0530

    school and courses
    Added 3 Schools and based on that courses with the help of Javascript
    Different schools will show different Courses offered by the schools
    Each(Two) <input> i.e. School and Courses has different theme to it

--- apply.php
+++ apply.php
@@ -41,11 +41,31 @@ $(function() {
     "School of Liberal Studies and Education"
   ];
   
-  $(".autocomplete").autocomplete({
+  $(".autocomplete").autocomplete({ 
     source: availableTags
   });
 });
+
+$(function(){
+    $('#groups').on('change', function(){
+        var val = $(this).val();
+        var sub = $('#sub_groups');
+        $('option', sub).filter(function(){
+            if (
+                 $(this).attr('data-group') === val 
+              || $(this).attr('data-group') === 'SHOW'
+            ) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+    $('#groups').trigger('change');
+});
 </script>
+
+
   <div class="container">
     <div class="col-lg-9">         
       <div class="panel panel-default">
@@ -62,12 +82,39 @@ $(function() {
 
             <div class="col-sm-12 form-group ui-widget">
             <label>School</label>
-                <input type="text" name="fullname" class="form-control autocomplete" required>
+                <input type="text" name="fullname" class="form-control autocomplete" id="groups" required>
             </div>
 
             <div class="col-sm-12 form-group">
                     <label>Courses</label>
-                    <input type="text" name="fullname" class="form-control" required>
+                    
+                    <select id="sub_groups" class="form-control">
+                        <option active>Select a Course</option>
+
+                        <option data-group='School of Business and Law' value='BCA'>Bachelor of Computer Applications </option>
+                        <option data-group='School of Business and Law' value='BBA'>Bachelor of Bussiness Administration </option>
+                        <option data-group='School of Business and Law' value='BBA-LLB'>Bachelor of Bussiness Administration-LLB </option>
+                        <option data-group='School of Business and Law' value='MBA'>Master of Bussiness Administration-LLB </option>
+                        <option data-group='School of Business and Law' value='MSC(IT)'>Master of Science (IT) </option>
+                        <option data-group='School of Business and Law' value='MHRM'>Master of Human Resource Management</option>
+
+                        <option data-group='School of Environmental Design and Architecture' value='BArch'> Bachelor of Architecture </option>
+                        <option data-group='School of Environmental Design and Architecture' value='BDesign'> Bachelor of Design </option>
+                        <option data-group='School of Environmental Design and Architecture' value='BLAD'>Bachelor Of Landscape Architectural Design Program </option>
+
+                        
+                        <option data-group='School of Liberal Studies and Education' value='Bscp'>Bachelor of Science+ </option>
+                        <option data-group='School of Liberal Studies and Education' value='BscCMB'>B.Sc-M.Sc Integrated in Biomedical Science </option>
+                        <option data-group='School of Liberal Studies and Education' value='BA'> Bachelor Of Arts (Journalism And Mass Communication) </option>
+                        <option data-group='School of Liberal Studies and Education' value='BEd'>Bachelor of Education </option>
+                        <option data-group='School of Liberal Studies and Education' value='MSC(CHM)'>Master of Science Chemistry</option>
+                        <option data-group='School of Liberal Studies and Education' value='MSC(L.S)'>M.Sc. Life Science</option>
+                        <option data-group='School of Liberal Studies and Education' value='MSW'>MSW Program</option>
+                        <option data-group='School of Liberal Studies and Education' value='MEd'>Master of Education</option>
+                        <option data-group='School of Liberal Studies and Education' value='ECCE'>P G Diploma in Early Childhood Care and Education</option>
+                           
+                    </select> 
+                   <!-- <input type="text" name="fullname" class="form-control" required> -->
             </div>
 
             </form>
