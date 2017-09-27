d15ce6a83256f74aa8714586b9d9ebcefaceedd5
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-28 01:24:45 +0530

    temporary solution
    Added a temoprary fix for 404 error message

--- 404.php
+++ 404.php
@@ -1,21 +1,13 @@
 <html>
-<?php include 'home-menu.php'; ?>
-    <?php include 'user-side-menu.php' ; ?>
-
-        <div class="col-md-9">
-            <!-- Website Overview -->
+<?php include 'menu.php'; ?>
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading main-color-bg">
-                    <h3 class="panel-title">404 Page Not Found</h3>
+                    <h3 class="panel-title"><center>404 Page Not Found<center></h3>
                 </div>
                 <div class="panel-body">
-                        404 Page Not Found
+                        <center> The page you're looking for does not exist or you may have made a typo. Click <a href="index.php">here</a> to go home page </center>
                 </div>
             </div>
         </div>
-        
-<!-- To add same code before the last div -- >
-
-
-
 </html>
