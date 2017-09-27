75267a5798ac44b2bdc20dd1f8f6fa0015116f54
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-28 01:12:52 +0530

    404
    Working on Page not found error, however cannot find solution it for apache server

---
+++ 404.php
@@ -0,0 +1,21 @@
+<html>
+<?php include 'home-menu.php'; ?>
+    <?php include 'user-side-menu.php' ; ?>
+
+        <div class="col-md-9">
+            <!-- Website Overview -->
+            <div class="panel panel-default">
+                <div class="panel-heading main-color-bg">
+                    <h3 class="panel-title">404 Page Not Found</h3>
+                </div>
+                <div class="panel-body">
+                        404 Page Not Found
+                </div>
+            </div>
+        </div>
+        
+<!-- To add same code before the last div -- >
+
+
+
+</html>
