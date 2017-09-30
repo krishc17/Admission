ac39d7fe6a9489c6b0e4f12f10bf203c533e454b
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-30 20:13:27 +0530

    external js and css
    made external js and css of apply page

--- apply.php
+++ apply.php
@@ -2,66 +2,10 @@
 <html lang="en">
 	<?php include 'home-menu.php'; ?>
 	<?php include 'user-side-menu.php'; ?>
+	<link rel="stylesheet" href="css/apply.css" type="text/css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
-	<style>
-		.ui-autocomplete {
-		position: absolute;
-		z-index: 1000;
-		cursor: default;
-		padding: 0;
-		margin-top: 2px;
-		list-style: none;
-		background-color: #ffffff;
-		border: 1px solid #ccc
-		-webkit-border-radius: 5px;
-		-moz-border-radius: 5px;
-		border-radius: 5px;
-		-webkit-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-		-moz-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-		box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-		}
-		.ui-autocomplete > li {
-		padding: 3px 20px;
-		}
-		.ui-autocomplete > li.ui-state-focus {
-		background-color: #DDD;
-		}
-		.ui-helper-hidden-accessible {
-		display: none;
-		}
-	</style>
-	<script>  
-		$(function() {
-		  var availableTags = [
-		    "School of Business and Law", 
-		    "School of Environmental Design and Architecture", 
-		    "School of Liberal Studies and Education"
-		  ];
-		  
-		  $(".autocomplete").autocomplete({ 
-		    source: availableTags
-		  });
-		});
-		
-		$(function(){
-		    $('#groups').on('change', function(){
-		        var val = $(this).val();
-		        var sub = $('#sub_groups');
-		        $('option', sub).filter(function(){
-		            if (
-		                 $(this).attr('data-group') === val 
-		              || $(this).attr('data-group') === 'SHOW'
-		            ) {
-		                $(this).show();
-		            } else {
-		                $(this).hide();
-		            }
-		        });
-		    });
-		    $('#groups').trigger('change');
-		});
-	</script>
+	<script src="js/apply.js"></script>
 	<div class="container">
 		<div class="col-lg-9">
 			<div class="panel panel-default">
