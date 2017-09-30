4da1e6f92c3fb5df4b5e995e33c04631f22ddf03
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-30 12:26:41 +0530

    css returns
    removed css from php file and moved it ro login.css for
    beatufied code again

--- home.php
+++ home.php
@@ -1,52 +1,55 @@
 <html>
 <?php include 'dbConfig.php'?>
 <?php include 'home-menu.php'; ?>
-    <?php include 'user-side-menu.php' ; ?>
-
-        <div class="col-md-9">
-            <!-- Website Overview -->
-            <div class="panel panel-default">
-                <div class="panel-heading main-color-bg">
-                    <h3 class="panel-title">Instructions</h3>
-                </div>
-                <div class="panel-body">
-                    <p>1. Once registered on the portal you can apply for individual Courses given in the options </p>
-                    <p>2. You'll have to submit your educational details in your profile in the 'Profile' section. </p>
-                    <p>3. After submitting the educational details apply for Course. </p>
-                    <p>4. In course selection select the respective School and then the respective course </p>
-                </div>
-            </div>
-        </div>
-        
+<?php include 'user-side-menu.php' ; ?>
+<div class="col-md-9">
+  <!-- Website Overview -->
+  <div class="panel panel-default">
+    <div class="panel-heading main-color-bg">
+      <h3 class="panel-title">Instructions
+      </h3>
+    </div>
+    <div class="panel-body">
+      <p>1. Once registered on the portal you can apply for individual Courses given in the options 
+      </p>
+      <p>2. You'll have to submit your educational details in your profile in the 'Profile' section. 
+      </p>
+      <p>3. After submitting the educational details apply for Course. 
+      </p>
+      <p>4. In course selection select the respective School and then the respective course 
+      </p>
+    </div>
+  </div>
+</div>
 <!-- To add same code before the last div -- >
-
-
-        <!-- Payment Instructions -->
-        <div class="col-md-9">
-            <div class="panel panel-default">
-                <div class="panel-heading main-color-bg">
-                    <h3 class="panel-title">Admission Instructions</h3>
-                </div>
-                <div class="panel-body">
-                    <p> 1. </p>
-                    <p> 2.  </p>
-                    <p> 3. </p>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-            <div class="panel-heading main-color-bg">
-                    <h3 class="panel-title">Payment Instructions</h3>
-                </div>
-                <div class="panel-body">
-                    <p> 1. Form Fees : Rs. 1000/- Nonrefundable ( payment mode : online by master/visa card OR offline at university ) </p>
-                    <p> 2. Do not refresh (F5)form while filling data. </p>
-                </div>
-            </div>
-
-            </div>
-        </div>
-
-
-
+<!-- Payment Instructions -->
+<div class="col-md-9">
+  <div class="panel panel-default">
+    <div class="panel-heading main-color-bg">
+      <h3 class="panel-title">Admission Instructions
+      </h3>
+    </div>
+    <div class="panel-body">
+      <p> 1. 
+      </p>
+      <p> 2.  
+      </p>
+      <p> 3. 
+      </p>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading main-color-bg">
+      <h3 class="panel-title">Payment Instructions
+      </h3>
+    </div>
+    <div class="panel-body">
+      <p> 1. Form Fees : Rs. 1000/- Nonrefundable ( payment mode : online by master/visa card OR offline at university ) 
+      </p>
+      <p> 2. Do not refresh (F5)form while filling data. 
+      </p>
+    </div>
+  </div>
+</div>
+</div>
 </html>
--- register.php
+++ register.php
@@ -3,86 +3,86 @@
 <?php include 'menu.php'; ?>
 <link rel="stylesheet" type="text/css" href="css/regForm.css">
 <body>
-  <div class="container">
-	<h1 class="well">Registration Form
-	</h1>
-	<div class="col-lg-12 well">
-	  <div class="row">
-		<form action="dbConfig.php" method="post">
-		  <div class="col-sm-12">
+	<div class="container">
+		<h1 class="well">Registration Form
+		</h1>
+		<div class="col-lg-12 well">
 			<div class="row">
-			  <div class="col-sm-12 form-group">
-				<label>Fullname
-				</label>
-				<input type="text" name="fullname" placeholder="Enter Full Name Here.." class="form-control" required>
-			  </div>
+				<form action="dbConfig.php" method="post">
+					<div class="col-sm-12">
+						<div class="row">
+							<div class="col-sm-12 form-group">
+								<label>Fullname
+								</label>
+								<input type="text" name="fullname" placeholder="Enter Full Name Here.." class="form-control" required>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-6 form-group">
+								<label>Gender
+								</label>
+								<input list="gender" name="gender" class="form-control" placeholder="Select Gender" required>
+								<datalist id="gender">
+									<option value="Male">
+									<option value="Female">
+								</datalist>
+							</div>
+							<div class="col-sm-6 form-group">
+								<label>Blood Group
+								</label>
+								<input type="text" name="bgroup" placeholder="Enter Bloodgroup..(B+VE or B-VE)" class="form-control" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label>Address
+							</label>
+							<textarea name="address" placeholder="Enter Address Here.." rows="3" class="form-control" required>
+							</textarea>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 form-group">
+								<label>City
+								</label>
+								<input type="text" name="city" placeholder="Enter City Name Here.." class="form-control" required>
+							</div>
+							<div class="col-sm-4 form-group">
+								<label>State
+								</label>
+								<input type="text" name="state" placeholder="Enter State Name Here.." class="form-control" required>
+							</div>
+							<div class="col-sm-4 form-group">
+								<label>Zip
+								</label>
+								<input type="text"  name="zip" placeholder="Enter Zip Code Here.." class="form-control" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label>Phone Number
+							</label>
+							<input type="text" name="pnumber" placeholder="Enter Phone Number Here.." class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Email Address
+							</label>
+							<input type="email" name="email" placeholder="Enter Email Address Here.." class="form-control" required>
+						</div>
+						<div class="row">
+							<div class="col-sm-6 form-group">
+								<label>Password
+								</label>
+								<input name="password" type="password" placeholder="Enter Password.." class="form-control" required>
+							</div>
+							<div class="col-sm-6 form-group">
+								<label>Confirm Password
+								</label>
+								<input type="password" name="password2" placeholder="Confirm Password.." class="form-control" required>
+							</div>
+						</div>
+						<input class="btn btn-lg btn-info" type="submit" name="addRegister"  value="Submit">
+					</div>
+				</form>
 			</div>
-			<div class="row">
-			  <div class="col-sm-6 form-group">
-				<label>Gender
-				</label>
-				<input list="gender" name="gender" class="form-control" placeholder="Select Gender" required>
-				<datalist id="gender">
-				  <option value="Male">
-				  <option value="Female">
-				</datalist>
-			  </div>
-			  <div class="col-sm-6 form-group">
-				<label>Blood Group
-				</label>
-				<input type="text" name="bgroup" placeholder="Enter Bloodgroup..(B+VE or B-VE)" class="form-control">
-			  </div>
-			</div>
-			<div class="form-group">
-			  <label>Address
-			  </label>
-			  <textarea name="address" placeholder="Enter Address Here.." rows="3" class="form-control">
-			  </textarea>
-			</div>
-			<div class="row">
-			  <div class="col-sm-4 form-group">
-				<label>City
-				</label>
-				<input type="text" name="city" placeholder="Enter City Name Here.." class="form-control">
-			  </div>
-			  <div class="col-sm-4 form-group">
-				<label>State
-				</label>
-				<input type="text" name="state" placeholder="Enter State Name Here.." class="form-control">
-			  </div>
-			  <div class="col-sm-4 form-group">
-				<label>Zip
-				</label>
-				<input type="text"  name="zip" placeholder="Enter Zip Code Here.." class="form-control">
-			  </div>
-			</div>
-			<div class="form-group">
-			  <label>Phone Number
-			  </label>
-			  <input type="text" name="pnumber" placeholder="Enter Phone Number Here.." class="form-control">
-			</div>
-			<div class="form-group">
-			  <label>Email Address
-			  </label>
-			  <input type="email" name="email" placeholder="Enter Email Address Here.." class="form-control">
-			</div>
-			<div class="row">
-			  <div class="col-sm-6 form-group">
-				<label>Password
-				</label>
-				<input name="password" type="password" placeholder="Enter Password.." class="form-control">
-			  </div>
-			  <div class="col-sm-6 form-group">
-				<label>Confirm Password
-				</label>
-				<input type="password" name="password2" placeholder="Confirm Password.." class="form-control">
-			  </div>
-			</div>
-			<input class="btn btn-lg btn-info" type="submit" name="addRegister"  value="Submit">
-		  </div>
-		</form>
-	  </div>
+		</div>
 	</div>
-  </div>
 </body>
 </html>
