3c623087394bb17f14c66c259f88ed2ef77e0f43
dex.papa@gmail.com <dex.papa@gmail.com>
2017-09-30 09:23:04 +0530

    code
    made messy code in readable format.
    
    
    Signed-off-by: dex.papa@gmail.com <dex.papa@gmail.com>

--- dbConfig.php
+++ dbConfig.php
@@ -1,56 +1,60 @@
 <?php
-	@ob_start();
-	session_start();
-	// Database Connection
-	$databaseHost = "localhost";
-	$databaseUsername = "root";
-	$databasePassword = "";
-	$databaseName = "admission2018";
+@ob_start();
+session_start();
 
+// Database Connection
 
-	try{
-		//PDO Style
-		$conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName;",$databaseUsername,$databasePassword);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		echo "Connected successfully";
+$databaseHost = "localhost";
+$databaseUsername = "root";
+$databasePassword = "";
+$databaseName = "admission2018";
+try
+	{
+
+	// PDO Style
+
+	$conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName;", $databaseUsername, $databasePassword);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully";
 
 	// User Registeration Page
-	if(isset($_POST['addRegister']))
-	{
-    
-            $fullname = $_POST['fullname'];
-            $gender = $_POST['gender'];
-            $bgroup = $_POST['bgroup'];
-            $address = $_POST['address'];
-            $city =  $_POST['city'];
-            $state = $_POST['state'];
-            $zip = $_POST['zip'];
-            $pnumber = $_POST['pnumber'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-			$password2 = $_POST['password2'];
-	
-			if($password == $password2)
-			{
-                $password = md5($password);
-				//PDO Style Insert
 
-				$sql = "INSERT INTO `student_data` VALUES 
+	if (isset($_POST['addRegister']))
+		{
+		$fullname = $_POST['fullname'];
+		$gender = $_POST['gender'];
+		$bgroup = $_POST['bgroup'];
+		$address = $_POST['address'];
+		$city = $_POST['city'];
+		$state = $_POST['state'];
+		$zip = $_POST['zip'];
+		$pnumber = $_POST['pnumber'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$password2 = $_POST['password2'];
+		if ($password == $password2)
+			{
+			$password = md5($password);
+
+			// PDO Style Insert
+
+			$sql = "INSERT INTO `student_data` VALUES 
 				(NULL,'$fullname','$gender','$bgroup','$address','$city','$state','$zip','$pnumber','$email','$password')";
-				if($conn->query($sql))
+			if ($conn->query($sql))
 				{
-					echo "Registration Successful";
+				echo "Registration Successful";
 				}
-				else
+			  else
 				{
-					echo "An Error Occured Contact SysAdmin";	
-				}	
+				echo "An Error Occured Contact SysAdmin";
+				}
+			}
 		}
 	}
-}
-		catch(PDOException $e)
-		{
-		echo "Connection failed: " . $e->getMessage();
-		}
+
+catch(PDOException $e)
+	{
+	echo "Connection failed: " . $e->getMessage();
+	}
 
 ?>
