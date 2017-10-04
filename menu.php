599e61576161e24d40caac256edd7724ed9030de
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-04 22:56:10 +0530

    index fix
    temporary index fix

--- menu.php
+++ menu.php
@@ -1,8 +1,12 @@
-<!-- No Need of session_start(); just yet -->``
+<!-- No Need of session_start(); just yet -->
+<a href="index.php">
+  <img src="img/nuv_logo.png" />
+  </a>
 <title> Navrachna Admission Portal </title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="style.css" type="text/css">
-<link rel="stylesheet" href="css/login.css" type="text/css">
+<link rel="stylesheet" href="css/nuv-color.css" type="text/css">
+<link rel="stylesheet" href="css/login.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script>
@@ -11,11 +15,8 @@ $(document).ready(function(){
 });
 </script>
 
-<nav class="navbar navbar-inverse text-center h4">
+<div class="navbar-nuvcolor  text-center h4">
 <div class="container-fluid">
-  <a class="navbar-brand" href="index.php">
-  <img class="img" src="img/nuv_logo.png" />
-  </a>
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
     <span class="icon-bar">
@@ -25,8 +26,7 @@ $(document).ready(function(){
     <span class="icon-bar">
     </span>
     </button>
-    <a class="navbar-brand" href="index.php">Uni Logo
-    </a>
+
   </div>
   <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav vertical" style="text-decoration:none;">
@@ -51,7 +51,7 @@ $(document).ready(function(){
     </ul>
   </div>
 </div>
-</nav>
+</div>
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
 <div class="modal-dialog">
   <div class="loginmodal-container">
