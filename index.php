f84423174154a6be6ec4aff8a311e4814a33e826
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-07 12:46:44 +0530

    adding information
    adding textual information on the home page

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<html>
-	     <?php include 'menu.php'; ?>
-</html>
+<?php include 'menu.php'; ?>
+<!--start-->
+<div class="col-md-12">
+  <div class="panel panel-default">
+    <div class="panel-heading main-color-bg">
+      <h3 class="panel-title">The University</h3>
+    </div>
+    <div class="panel-body">
+	<p>
+	Navrachana University was established through the Gujarat Private Universities Act, 2009 with the intent to offer superior education that befits the high educational standards of the Navrachana Educational Society in Vadodara. The University has embarked on a new educational paradigm that lays simultaneous emphasis on disciplinary education, inter-disciplinary education, professional education and general education. This paradigm is facilitated by the University's unitary character and offers comprehensive learning and helps develop T-Shape students who have a breadth of knowledge though general and inter-disciplinary education and a depth of knowledge though focused disciplinary education.
+	</p>
+
+	<p>There are five important aspects that directs the new educational paradigm at Navrachana University: First, professional education must prepare students with strong disciplinary insights needed to address the challenges confronting the professional community and inculcate a sense of lifelong learning to be successful and excel overtime. Second, offer interdisciplinary education that cross-fertilize learning with new ideas from desperate disciplines so collision at the fringes raise appropriate questions and create innovation. Third, general education must inform students about the issues that arise in their lives, personally, professionally, and socially and help them to be reflective about their beliefs and choices, and their presuppositions and motivations. Fourth, make the process of learning more effective through exposure into real working conditions by practically applying knowledge and skills learned in the classroom. Fifth, invite students to work on "messy", seemingly intractable problems through a focus on social entrepreneurship and address pressing problems in our immediate community. Disciplinary-specific entrepreneurship is being developed to create job creators who will create opportunities for themselves and for others</p>
+
+	<p>Four schools make up the Navrachana University - School of Liberal Studies and Education with BSc., BA(Journalism & Mass Communication), BEd., MEd., ECCE, MSW, MSc(Chemistry) and PhD, School of Environmental Design and Architecture provides Bachelor of Architecture and Bachelor of Design (Interior); School of Engineering and Technology offers Bachelor of Mechanical, Electrical, Civil and Computer Science and Engineering, and School of Business and Law offers BBA, BBA-Law, MBA, BCA, and MSc(IT). All Educational Programs follow modular, semester-wise curricula and allow taking courses that cut across program boundaries. The unitary character allows the University to innovate new educational and pedagogic models grounded in interdisciplinary thinking and capitalize on the shared infrastructural and educational resources to achieve a rich academic and life experience.</p>
+    </div>
+  </div>
+</div>
+<!--end-->
+
+<!--start-->
+<div class="col-md-12">
+  <div class="panel panel-default">
+    <div class="panel-heading main-color-bg">
+      <h3 class="panel-title">The Campus</h3>
+    </div>
+    <div class="panel-body">
+		<p>
+		The campus of the Navrachana University is located in the city of Vadodara on Vasna-Bhayli Road and has excellent infrastructure with well stocked library, state-of-the-art computer labs, high-technology science and engineering labs, well-equipped lecture halls with projection technology and web-connected lecterns, Wi-Fi access throughout the campus, a hostel for male and female students, and a cafeteria that offers a wide range of food choices. The campus offers a place for education, work, interaction, fun and constant debate, and this is facilitated through the activities place into the spill-over places like the corridors, canteen and places across the stairs and lifts. These places serve as the hub of knowledge, where discussions take place and students interact over classroom projects and informal discussions. The education paradigm grounded in interdisciplinary and interactive education provokes every student reach out to others and work with them on educational, cultural and social activities, to take a step into the unknown, develop an inquiry in the mind, feel the urge to resolve and work collaboratively.
+		</p>
+    </div>
+  </div>
+</div>
+<!--end-->
+
