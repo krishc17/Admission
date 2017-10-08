fef9f345f2427fa13f9bfe43902f1ef247568b8b
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-08 18:43:48 +0530

    SBL
    Added description about courses offered by SBL and prototyping some things

--- courses.php
+++ courses.php
@@ -7,8 +7,139 @@
                 <h3 class="panel-title"> School of Business and Law</h3>
             </div>
             <div class="panel-body">
-
                 Working in close association with the industrial and academic experts, right from Curriculum development to student's projects, internships and work placements, School of Business and Law at NUV offers several unique under-graduate and post-graduate programs in the field of Management (BBA and MBA), Law (BBA-LLB Integrated), Human Resources (MHRM), and Computer Science and Applications (BCA and MScIT).
+                <br>
+                <br>
+                <br>
+                <div style="margin:0 auto" class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading main-color-bg">
+                            <h3 class="panel-title">Courses</h3>
+                        </div>
+                        <div class="panel-body">
+                            <!-- start SBL Courses -->
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title"> <a href="http://nuv.ac.in/bca_program.php" target="_blank" style="color:red;" title="Follow the link for more description about BCA"> BCA </a></h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <li>Duration - 3 Years</li>
+                                        <li>Program Delivery - Full Time</li>
+                                        <li>Contact Hours - 6 X 5 (30 Hours per week)</li>
+                                        <li>Class size - 30 to 40</li>
+                                        <li>Social and Industrial Internships</li>
+                                        <li>Final Semester Industrial Project</li>
+                                        <li>Inter-disciplinary approach with electives from different disciplines</li>
+                                        <li>Placement Assistance</li>
+                                        <li>6 Semesters - 40 Courses - 150 credits</li>
+                                        <br> <li>Extra-mural activities to encourage students' interest in field of technology, art and culture</li>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title">BBA</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <li>6 Semesters - 40 Courses - 151 credits</li>
+                                        <li>Contact Hours - 5 x 5 (25 Hours per week)</li>
+                                        <li>Class size - 50 to 60</li>
+                                        <li>Inter-disciplinary approach with electives from different disciplines</li><br>
+                                        <li>Social and Corporate Internships</li>
+                                        <li>Electives in the field of - Marketing Finance OB and HRM Family Business Management Systems and ICT
+                                        </li><br>
+                                        <li>Final Semester Research Project</li>
+                                        <li>Placement Assistance</li>
+                                        <br>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title">BBA-LLB</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <li>Duration - 5 Years</li>
+                                        <li>Program Delivery - Full-Time</li>
+                                        <li>Contact Hours - 5 x 5 (25 Hours per week)</li>
+                                        <li>Class size - 40 to 50</li>
+                                        <li>Practice through Moot Court</li>
+                                        <li>Social Internship, Corporate Internship and Legal Internships at different levels</li>
+                                        <li>10 Semesters - 226 credits (144 credits in Law, 62 in Management, 20 in Liberal Education)</li>
+                                        <li>Specialization in contemporary fields</li>
+                                        <li>Inter-disciplinary approach with electives from different disciplines</li>
+                                        <li>Placement Assistance</li>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title">MHRM</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <li>Duration - 2years</li>
+                                        <li>Program Delivery- Full Time</li>
+                                        <li>Contact Hours- 25 hours per week</li>
+                                        <li>Social, Corporate Internships</li>
+                                        <li>Seminar Course</li>
+                                        <li>Semester Long Exhaustive Research Project</li>
+                                        <li>Placement Assistance</li>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title">MBA</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Duration - 2 Years</li>
+                                            <li>Program Delivery - Full -Time</li>
+                                            <li>Contact Hours - 25 Hours per week</li>
+                                            <li>Class size - 60</li>
+                                            <li>Social and Corporate Internships</li>
+                                            <li>4 Semesters - 8 Modules 45 Courses - 108 credits</li>
+                                            <li>MBA with Specialization in - - Marketing - Finance - OB and HRM - Family Business Management - Systems and ICT</li>
+                                            <li>Placement Assistance</li>
+
+                                        </ul>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading main-color-bg">
+                                        <h3 class="panel-title">Msc(IT)</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Duration - 2 Years , Full Time</li>
+                                            <li>Contact Hours- 30 Hours per wk</li>
+                                            <li>Class size - 30-40 (approx)</li>
+                                            <li>Industrial Internships</li>
+                                            <li>Year Long Software development Projects Advance level courses and a variety of electives in different specializations</li>
+                                            <li>Placement Assistance</li>
+                                            <li>4 semesters -28 Courses - 101 credits</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
