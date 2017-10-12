820f224c356485973b59c19a19835e91115ee87f
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-12 21:47:39 +0530

    welcome message
    added welcome {user} to menu bar

--- home/home-menu.php
+++ home/home-menu.php
@@ -1,3 +1,29 @@
+<?php
+
+	if(isset($_SESSION['email']))
+	{
+        $email = $_SESSION['email'];
+        $databaseHost = "localhost";
+        $databaseUsername = "root";
+        $databasePassword = "";
+        $databaseName = "admission2018";
+        
+        $mysqli = new mysqli($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+        $query = "SELECT * FROM student_data WHERE email = '{$_SESSION['email']}'"; 
+        $result = $mysqli->query($query) or die($mysqli->error);
+		if($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{
+				foreach($row as $val) 
+				{
+                    $details[] = $val;
+                }
+            }   
+		}
+	}
+     ?>
+
 <title>
 		Navrachna Admission Portal
 </title>
@@ -21,6 +47,7 @@
 							<li><a class="link-3" href="apply.php">Apply</a></li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right horizontal">
+								<p class="navbar-brand">Welcome <?php echo $details[1];?></p>
 								<a href="logout.php" class="navbar-brand" >Logout</a>
 						</ul>
 				</div>
