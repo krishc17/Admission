2d861341c35d6abd24948719aa7c7a1e5f538487
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-15 09:32:02 +0530

    responsive
    added meta tag to make admin login responsive

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <title>Admin Area | Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/login.css">
 <div class="modal-dialog login-center"> 
   <div class="loginmodal-container">
--- menu.php
+++ menu.php
@@ -2,10 +2,10 @@
 <a href="index.php"> <img src="img/nuv_logo.png" /> </a>
 <title> Navrachna Admission Portal </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css" type="text/css">
-  <link rel="stylesheet" href="css/nuv-color.css" type="text/css">
-  <link rel="stylesheet" href="css/login.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="stylesheet" href="css/nuv-color.css" type="text/css">
+<link rel="stylesheet" href="css/login.css">
   
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
