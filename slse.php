aa60d74225d91d601edcd5e483c50f404e6baab6
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-19 11:33:01 +0530

    code beautify
    beatutified the code

--- slse.php
+++ slse.php
@@ -6,101 +6,88 @@
             <h3 class="panel-title">School of Business and Law</h3>
         </div>
         <div class="panel-body">
-            Working in close association with the industrial and academic experts, right from Curriculum development to student's projects, internships and work placements, School of Business and Law at NUV offers several unique under-graduate and post-graduate programs in the field of Management (BBA and MBA), Law (BBA-LLB Integrated), Human Resources (MHRM), and Computer Science and Applications (BCA and MScIT)    
+            Working in close association with the industrial and academic experts, right from Curriculum development to student's projects, internships and work placements, School of Business and Law at NUV offers several unique under-graduate and post-graduate programs in the field of Management (BBA and MBA), Law (BBA-LLB Integrated), Human Resources (MHRM), and Computer Science and Applications (BCA and MScIT)
             <br>
-                <br/>
-                <br/    >
-                    <div class="col-md-12">
-                        <div id="accordion">
-                            <div class="panel panel-default">
-                                <div class="panel-heading main-color-bg">
-                                    <h3 class="panel-title">
-                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Courses</a>
-                                    </h3>
+            <br/>
+            <br/>
+            <div class="col-md-12">
+                <div id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading main-color-bg">
+                            <h3 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Courses</a>
+                            </h3>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <!-- start SBL Courses -->
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> BBA </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <!-- content -->
+                                        </div>
+                                    </div>
                                 </div>
-                                <div id="collapseOne" class="panel-collapse collapse in">
-                                    <div class="panel-body">
-                                        <!-- start SBL Courses -->
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  BBA </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
-                                            </div>
+                                
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> BCA </h3>
                                         </div>
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  BCA </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
-
-                                                
-                                            </div>
-                                            
+                                        <div class="panel-body">
+                                            <!-- content -->
                                         </div>
+                                    </div>
+                                </div>
 
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  BBA-LLB </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
-
-                                                
-                                            </div>
-                                            
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> BBA-LLB </h3>
                                         </div>
-
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  MBA </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
-
-                                                
-                                            </div>
-                                            
+                                        <div class="panel-body">
+                                            <!-- content -->
                                         </div>
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  MCS(IT) </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
+                                    </div>
+                                </div>
 
-                                                
-                                            </div>
-                                            
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> MBA </h3>
                                         </div>
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default">
-                                                <div class="panel-heading main-color-bg">
-                                                    <h3 class="panel-title">  MHRM </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <!-- content -->
-                                                </div>
-
-                                                
-                                            </div>
-                                            
+                                        <div class="panel-body">
+                                            <!-- content -->
                                         </div>
+                                    </div>
+                                </div>
 
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> MCS(IT) </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <!-- content -->
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading main-color-bg">
+                                            <h3 class="panel-title"> MHRM </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <!-- content -->
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!--end-->
+                    </div>
+                </div>
+                <!--end-->
