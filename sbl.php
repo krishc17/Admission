b3ba9ef6b1c7022bac584104b8c17047a74c4ade
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-19 11:43:09 +0530

    content
    added content for SBL and beautified the code

--- sbl.php
+++ sbl.php
@@ -27,18 +27,24 @@
                                             <h3 class="panel-title"> BBA </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            <p>6 Semesters - 40 Courses - 151 credits</p>
+                                            <p>Contact Hours - 5 x 5 (25 Hours per week)</p>
+                                            <p>Class size - 50 to 60</p>
+                                            <p>Inter-disciplinary approach with electives from different disciplines</p>
+                                            <p>Social and Corporate Internships</p>
+                                            <p>Final Semester Research Project</p>
+                                            <p>Placement Assistance</p>
                                         </div>
                                     </div>
                                 </div>
-                                
+
                                 <div class="col-md-4">
                                     <div class="panel panel-default">
                                         <div class="panel-heading main-color-bg">
                                             <h3 class="panel-title"> BCA </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            <p>Bachelor of Computer Applications (BCA) is a 3 year program designed to meet changing requirements of the IT sector and to produce a dynamic breed of computer professionals with excellent managerial skills. This is a unique program focusing on hands-on experience on different technologies along with inter-disciplinary project "Khoj". The BCA education is broad based with courses from inter-disciplinary area and management helping students for entry level positions in ICT industry or setting up own technical ventures.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -49,7 +55,9 @@
                                             <h3 class="panel-title"> BBA-LLB </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            <p>
+                                                BBA-LLB is a 5-Year Integrated degree program designed with an objective of providing students a thorough knowledge in the field of management education and law. The program offers specialization in corporate law, banking law, intellectual property rights, environmental laws and labor laws leading to a lucrative career in practicing law as an advocate or as a legal advisor. The program is duly approved by the Bar Council of India (BCI) and has been designed as per their guidelines. In addition to the regular Management and Law courses, the BBA-LLB program at NUV offers various compulsory and elective courses in the field of communication and liberal education. The focus of the program is to widen students' knowledge through connecting Business Theory and Law to practice and making it relevant to their overall career objective.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -60,7 +68,7 @@
                                             <h3 class="panel-title"> MBA </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            Well-structured classroom sessions, and personal interaction with faculty Short case studies in management and other functional areas Project work as application of concepts to real life situations Interaction with business leaders though Workshops and talks by Experts Experiential learning though visits and interaction at selected business units
                                         </div>
                                     </div>
                                 </div>
@@ -68,10 +76,12 @@
                                 <div class="col-md-4">
                                     <div class="panel panel-default">
                                         <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MCS(IT) </h3>
+                                            <h3 class="panel-title"> MSC(IT) </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            <p>
+                                                Master of Science (IT) is 2 year program specially designed to meet the technological challenges of the IT industry. Continuously updated curriculum provides insight into the technological trends of the IT sector and hands-on experience in different application domains. The program allows focusing in upcoming business domains of the IT sector like business analysis, specializing in specific business functional domain, business intelligence and cloud computing.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -82,7 +92,9 @@
                                             <h3 class="panel-title"> MHRM </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                            <p>
+                                                MHRM is an appropriate degree for students who are interested in advancing their careers in human resource management. This program is highly focused, covering topics such as Contemporary Talent acquisition and management strategies, new approaches to worker involvement, labour law and strategic human resource management. It prepares students for success as human resource professionals in a variety of sectors including private industry, services, government, education and healthcare
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
