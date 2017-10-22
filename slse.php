acd19dd9efc185d15bafcfe0ec0b762381561fe9
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-22 12:18:53 +0530

    beautify
    beautified the code

--- slse.php
+++ slse.php
@@ -1,131 +1,128 @@
 <?php include 'menu.php'; ?>
 <!--start-->
 <div class="col-md-12">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">School of Liberal Studies and Education</h3>
-        </div>
-        <div class="panel-body">
-        Programs offered under Liberal Studies are Bachelor of Arts (Journalism & Mass Communication), Bachelor of Science (BSc+), Master of Science (Chemistry) and Master of Social Work (MSW). 
-        Under Teacher Education programs offered are: Bachelor of Education (BEd.), Master of Education (MEd.), Post graduate Diploma in Early Childhood Care and Education (ECCE) and Ph D (Education).
-        Well-known for its programs in teacher education which are NCTE approved, the School has a well-earned reputation in the field of teacher education due to its high quality curriculum, practice teaching and faculty research. The university, which has its roots in top quality schools, is well connected with the local education community and ensures that the knowledge and skills essential for a modern day teacher are adequately developed through its courses and successfully applied to the needs of schools and society. 
-        BEd has an excellent record (100%) in placement over the last eleven years
-            <br/>
-            <br/>
-            <br/>
-            <div class="col-md-12">
-                <div id="accordion">
-                    <div class="panel panel-default">
-                        <div class="panel-heading main-color-bg">
-                            <h3 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Courses</a>
-                            </h3>
-                        </div>
-                        <div id="collapseOne" class="panel-collapse collapse in">
-                            <div class="panel-body">
-                                <!-- start SBL Courses -->
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BSc+ </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BSc - MSc </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">School of Liberal Studies and Education</h3>
+		</div>
+		<div class="panel-body">
+			Programs offered under Liberal Studies are Bachelor of Arts (Journalism & Mass Communication), Bachelor of Science (BSc+), Master of Science (Chemistry) and Master of Social Work (MSW). Under Teacher Education programs offered are: Bachelor of Education (BEd.), Master of Education (MEd.), Post graduate Diploma in Early Childhood Care and Education (ECCE) and Ph D (Education). Well-known for its programs in teacher education which are NCTE approved, the School has a well-earned reputation in the field of teacher education due to its high quality curriculum, practice teaching and faculty research. The university, which has its roots in top quality schools, is well connected with the local education community and ensures that the knowledge and skills essential for a modern day teacher are adequately developed through its courses and successfully applied to the needs of schools and society. BEd has an excellent record (100%) in placement over the last eleven years
+			<br/>
+			<br/>
+			<br/>
+			<div class="col-md-12">
+				<div id="accordion">
+					<div class="panel panel-default">
+						<div class="panel-heading main-color-bg">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Courses</a>
+							</h3>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in">
+							<div class="panel-body">
+								<!-- start SBL Courses -->
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> BSc+ </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BA </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> BSc - MSc </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BEd </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> BA </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MSc(Chem) </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> BEd </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MSc(LS) </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> MSc(Chem) </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MSW </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> MSc(LS) </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
+
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> MSW </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MEd </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> MEd </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> ECCE </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+								<div class="col-md-4">
+									<div class="panel panel-default">
+										<div class="panel-heading main-color-bg">
+											<h3 class="panel-title"> ECCE </h3>
+										</div>
+										<div class="panel-body">
+											<!-- content -->
+										</div>
+									</div>
+								</div>
 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--end-->
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--end-->
