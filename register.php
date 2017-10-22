5f52a93474086f265214c4af2855ac7b6e38bf58
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-22 21:22:21 +0530

    beautified
    beautified code and added list of bloodgroups

--- register.php
+++ register.php
@@ -2,10 +2,11 @@
 <?php include_once 'dbConfig.php'; ?>
 <?php include 'menu.php'; ?>
 <link rel="stylesheet" type="text/css" href="css/regForm.css">
+
 <body>
 	<div class="container">
 		<h1 class="well" style="text-align: center;">Registration Form</h1>
-			<?php echo $msg;?>
+		<?php echo $msg;?>
 		<div class="col-lg-12 well">
 			<div class="row">
 				<form action="register.php" enctype="multipart/form-data" method="post">
@@ -14,7 +15,7 @@
 							<div class="col-sm-6 form-group">
 								<label>Fullname</label>
 								<input type="text" name="fullname" placeholder="Enter Full Name Here.." class="form-control" required>
-							</div>	
+							</div>
 
 							<div class="col-sm-6 form-group">
 								<label>Image</label>
@@ -24,15 +25,23 @@
 						<div class="row">
 							<div class="col-sm-6 form-group">
 								<label>Gender </label>
-								<input list="gender" name="gender" class="form-control" placeholder="Select Gender" required>
-								<datalist id="gender">
+								<select id="gender" name="gender" placeholder="Select Gender" class="form-control" required>
 									<option value="Male"> Male </option>
 									<option value="Female"> Female </option>
-								</datalist>
+								</select>
 							</div>
 							<div class="col-sm-6 form-group">
 								<label>Blood Group</label>
-								<input type="text" name="bgroup" placeholder="Enter Bloodgroup..(B+VE or B-VE)" class="form-control" required>
+								<select id="bgroup" name="bgroupp" placeholder="Select Bloodgroup" class="form-control" required>
+									<option> A+VE </option>
+									<option> A-VE </option>
+									<option> B+VE </option>
+									<option> B-VE </option>
+									<option> O+VE </option>
+									<option> O-VE </option>
+									<option> AB+VE </option>
+									<option> AB-VE </option>
+								</select>
 							</div>
 						</div>
 						<div class="form-group">
@@ -41,12 +50,12 @@
 						</div>
 						<div class="row">
 							<div class="col-sm-3 form-group">
-                                <label>Date of Birth</label>
-                                <input type="text" data-format="dd/MM/yyy" id="datepicker"  placeholder="dd-MM-yyyy"  name="dob" class="form-control"  required>
+								<label>Date of Birth</label>
+								<input type="text" data-format="dd/MM/yyy" id="datepicker" placeholder="dd-MM-yyyy" name="dob" class="form-control" required>
 							</div>
 							<div class="col-sm-3 form-group">
 								<label>City</label>
-								<input type="text" name="city" placeholder="Enter City Name Here.."class="form-control" required>
+								<input type="text" name="city" placeholder="Enter City Name Here.." class="form-control" required>
 							</div>
 							<div class="col-sm-3 form-group">
 								<label>State</label>
@@ -54,7 +63,7 @@
 							</div>
 							<div class="col-sm-3 form-group">
 								<label>Zip</label>
-								<input type="text"  name="zip" placeholder="Enter Zip Code Here.." class="form-control" required>
+								<input type="text" name="zip" placeholder="Enter Zip Code Here.." class="form-control" required>
 							</div>
 						</div>
 						<div class="form-group">
@@ -75,11 +84,12 @@
 								<input type="password" name="password2" placeholder="Confirm Password.." class="form-control" required>
 							</div>
 						</div>
-						<input class="btn btn-lg btn-info" type="submit" name="addRegister"  value="Submit">
+						<input class="btn btn-lg btn-info" type="submit" name="addRegister" value="Submit">
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 </body>
+
 </html>
