902ed6996c8db119d7bff860b9942894d91e365b
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-22 19:38:32 +0530

    beautified

--- admin/records.php
+++ admin/records.php
@@ -5,54 +5,55 @@
       $email = $_SESSION['email'];
       include 'adminLoginHandler.php';    ?>
 <html lang="en">
+
 <body>
-   <!-- Almost Common for Every Page -->
-   <?php include'adminMenu.php' ?>
-   <header id="header">
-      <div class="container">
-         <div class="row">
-            <div class="col-md-10">
-               <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Search Records </h1>
-            </div>
-         </div>
-      </div>
-      </div>
-      </div>
-   </header>
-   <section id="breadcrumb">
-      <div class="container">
-         <ol class="breadcrumb">
-            <li class="active">Records</li>
-         </ol>
-      </div>
-   </section>
-   <?php include 'side-menu.php' ?>
-   <div class="col-md-9">
-      <!-- Website Overview -->
-      <div class="panel panel-default">
-         <div class="panel-heading main-color-bg">
-            <h3 class="panel-title">Admissions Record</h3>
-         </div>
-         <div class="panel-body">
-            <div class="row">
-            <form action="" method="post">
-               <div class="col-md-4">
-                  <div class="form-group">
-                     <label for="exampleSelect1">Select Year</label>
-                     <select class="form-control" id="exampleSelect1">
+	<!-- Almost Common for Every Page -->
+	<?php include'adminMenu.php' ?>
+	<header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10">
+					<h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Search Records </h1>
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
+	</header>
+	<section id="breadcrumb">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li class="active">Records</li>
+			</ol>
+		</div>
+	</section>
+	<?php include 'side-menu.php' ?>
+	<div class="col-md-9">
+		<!-- Website Overview -->
+		<div class="panel panel-default">
+			<div class="panel-heading main-color-bg">
+				<h3 class="panel-title">Admissions Record</h3>
+			</div>
+			<div class="panel-body">
+				<div class="row">
+					<form action="" method="post">
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleSelect1">Select Year</label>
+								<select class="form-control" id="exampleSelect1">
                         <option>2017</option>
                         <option>2018</option>
                         <option>2019</option>
                         <option>2020</option>
                         <option>2021</option>
                      </select>
-                  </div>
-               </div>
-               
-               <div class="col-md-4">
-                  <div class="form-group">
-                     <label for="exampleSelect1">Select Course</label>
-                     <select class='form-control' id='exampleSelect1'>
+							</div>
+						</div>
+
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleSelect1">Select Course</label>
+								<select class='form-control' id='exampleSelect1'>
                      <?php
                           $mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
                           $query = "SELECT * FROM courses";
@@ -63,26 +64,26 @@
                           }
                      ?>
                     </select>
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="form-group">
-                     <label>Search</label>
-                      <input type="submit" name="searchQuery" class="form-control" value="Search">
-                  </div>
-               </div>
-            </div>
-            </form>
-            <br>
-            <table class="table table-striped table-hover table-bordered">
-               <tr>
-                  <th>ID</th>
-                  <th>Student Name</th>
-                  <th>Email</th>
-                  <th>Course</th>
-                  <th>Joined On</th>
-               </tr>
-               <?php
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label>Search</label>
+								<input type="submit" name="searchQuery" class="form-control" value="Search">
+							</div>
+						</div>
+				</div>
+				</form>
+				<br>
+				<table class="table table-striped table-hover table-bordered">
+					<tr>
+						<th>ID</th>
+						<th>Student Name</th>
+						<th>Email</th>
+						<th>Course</th>
+						<th>Joined On</th>
+					</tr>
+					<?php
                 $mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
                 $query = "select student_data.id,student_data.FULLNAME,student_data.EMAIL,selected_courses.coursename,student_data.register_date FROM student_data JOIN selected_courses ON student_data.ID=selected_courses.ID";
                 $result = mysqli_query($mysqli,$query);       
@@ -101,20 +102,20 @@
                   // TODO: Echo Results of Query
                 }
                   ?>
-            </table>
-         </div>
-      </div>
-   </div>
-   </div>
-   </div>
-   </section>
-   <script>
-      CKEDITOR.replace( 'editor1' );
-   <?php
+				</table>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</section>
+	<script>
+	CKEDITOR.replace('editor1');
+	<?php
   }
 else{
-?> 
-  <?php
+?>
+	<?php
 echo 'not logged in ';
 }
 ?>
