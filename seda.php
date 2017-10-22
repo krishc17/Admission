d7ee82341909d0169e7541d11a0754da77ac241f
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-22 12:10:29 +0530

    seda
    added seda information

--- seda.php
+++ seda.php
@@ -3,10 +3,10 @@
 <div class="col-md-12">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <h3 class="panel-title">School of Business and Law</h3>
+            <h3 class="panel-title">School of Environmental Design and Architecture</h3>
         </div>
         <div class="panel-body">
-            Working in close association with the industrial and academic experts, right from Curriculum development to student's projects, internships and work placements, School of Business and Law at NUV offers several unique under-graduate and post-graduate programs in the field of Management (BBA and MBA), Law (BBA-LLB Integrated), Human Resources (MHRM), and Computer Science and Applications (BCA and MScIT)
+        The School of Environmental Design and Architecture (SEDA) is at the cutting-edge of ingenuity and it provides a dynamic and challenging environment for students to study architecture and design and fosters creativity, critical thinking, intellectual development, ability to respond to our natural environment and cater to spatial needs and aspirations of individuals and the society. The School has been established to offer broad-based Design Programs in Architecture and Interior Design with focused education to meet the challenges of the design profession.
             <br>
             <br/>
             <br/>
@@ -24,10 +24,10 @@
                                 <div class="col-md-4">
                                     <div class="panel panel-default">
                                         <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BBA </h3>
+                                            <h3 class="panel-title"> BArch </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                        Our Architecture program goes beyond just training students to fit into the professional slot. It introduces and develops the skills and knowledge necessary for contemporary architectural practice.
                                         </div>
                                     </div>
                                 </div>
@@ -35,10 +35,10 @@
                                 <div class="col-md-4">
                                     <div class="panel panel-default">
                                         <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BCA </h3>
+                                            <h3 class="panel-title"> BDesign </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
+                                        The B.Design (Interior) Program at Navrachana University is a full-time four year undergraduate program that offers a strong foundation in architecture while providing essential education necessary to succeed in Interior Design spaces, to offer architectural thinking to better understand building components, successfully develop and connect interior design components to Implementation, and succeed in multi-disciplinary teams that shape variety of careers in Interior Design and allied fields ranging from Space Planning, Residential, Corporate, and Retail Interiors, Furniture Design, Exhibition and Stage-set Designs.
                                         </div>
                                     </div>
                                 </div>
@@ -46,43 +46,13 @@
                                 <div class="col-md-4">
                                     <div class="panel panel-default">
                                         <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> BBA-LLB </h3>
+                                            <h3 class="panel-title"> BLAD </h3>
                                         </div>
                                         <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
+ 
+                                        The Bachelor course in Landscape Architectural Design (BLAD) is a full-time four year degree program. It deals with design and planning of Landscapes at various levels, from domestic to regional. 
 
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MBA </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MCS(IT) </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading main-color-bg">
-                                            <h3 class="panel-title"> MHRM </h3>
-                                        </div>
-                                        <div class="panel-body">
-                                            <!-- content -->
+The discipline of Landscape is concerned with design of Physical environment, similar to courses like B.Arch. and B. Design. It offers opportunity to shape not only the manmade environment the but also the natural environment. It is complementary to the discipline of Architecture and shares several common concerns. Hence, there are multiple collaborative possibilities offered between the two courses in form of studios and other subject
                                         </div>
                                     </div>
                                 </div>
