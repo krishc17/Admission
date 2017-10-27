cc60fa5ef0fbed26f47820c003b9c7d29030fda5
dex.papa@gmail.com <dex.papa@gmail.com>
2017-10-27 23:33:14 +0530

    beautify
    code beautify and minify

--- home/apply.php
+++ home/apply.php
@@ -45,8 +45,6 @@
 
 		}
 		else{
-
-
 		$selectedCourse = $_POST['selectedCourse'];
 		$id = $details[0];		
 		 $query = $conn->prepare("SELECT id,coursename from selected_courses where id=? AND coursename=?");			
@@ -74,54 +72,56 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<?php include 'home-menu.php';?>
-	<?php include 'user-side-menu.php'; ?>
-	<link rel="stylesheet" href="css/apply.css" type="text/css">
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
-	<script src="js/apply.js"></script>
-	<div class="container">
-		<div class="col-lg-9">
-			<div class="panel panel-default">
-				<div class="panel-heading main-color-bg">
-					<h3 class="panel-title">
-						<b>Apply For Course</b>
-					</h3>
-				</div>
-				<div class="panel-body">
-					<div class="col-lg-12">
-						<div class="row">
-							<div class="col-lg-10">
-								<form action="apply.php" method="post">
-									<div class="col-sm-12 form-group">
-										<label>Courses</label>
-										<select class="form-control" name="selectedCourse">
+<?php include 'home-menu.php';?>
+<?php include 'user-side-menu.php'; ?>
+<link rel="stylesheet" href="css/apply.css" type="text/css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
+<script src="js/apply.js"></script>
+<div class="container">
+    <div class="col-lg-9">
+        <div class="panel panel-default">
+            <div class="panel-heading main-color-bg">
+                <h3 class="panel-title">
+                    <b>Apply For Course</b>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-10">
+                            <form action="apply.php" method="post">
+                                <div class="col-sm-12 form-group">
+                                    <label>Courses</label>
+                                    <select class="form-control" name="selectedCourse">
 										<?php foreach($users as $user): ?>
         								<option value="<?= $user['coursename']; ?>"><?= $user['coursename'];?></option>
     									<?php endforeach; ?>
 										</select>
-									</div>
-									<input class="btn btn-info" type="submit" name="applyCourse"  value="Submit">
-								</form>
-								<p><?php echo $msg;?></p>
-							</div>
-							<div class="col-sm-12 form-group">
-							</div>
-						</div>
-
+                                </div>
+                                <input class="btn btn-info" type="submit" name="applyCourse" value="Submit">
+                            </form>
+                            <p>
+                                <?php echo $msg;?>
+                            </p>
+                        </div>
+                        <div class="col-sm-12 form-group">
+                        </div>
+                    </div>
 					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</html>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
 	}
 		else
 		{
-		?>	
-		<?php
+		?>
+<?php
 			header("Location:../index.php ");
 		}
 		?>
-	
+</html>
