00854ea658c67f811f4a01a8bda607ed7b9eb75a
dex.papa@gmail.com <dex.papa@gmail.com>
2017-11-03 12:02:11 +0530

    minimizing code

--- home/user-side-menu.php
+++ home/user-side-menu.php
@@ -15,8 +15,6 @@
       <a href="profile.php" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Profile<span class="badge"></span></a>
       <a href="educational_details.php" class="list-group-item"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Educational Details <span class="badge"></span></a>
       <a href="educational_details_be.php" class="list-group-item"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Educational Details (B.E) <span class="badge"></span></a>
-      <a href="apply.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Apply<span class="badge"></span></a>
-      <a href="user-test.php" class="list-group-item"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Entrance Test <span class="badge"></span></a>
-      <a href="logout.php" class="list-group-item"><span     aria-hidden="true"></span> Logout <span class="badge"></span></a>
+      <a href="apply.php" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Apply<span class="badge"></span></a>      <a href="logout.php" class="list-group-item"><span     aria-hidden="true"></span> Logout <span class="badge"></span></a>
    </div>
 </div>
