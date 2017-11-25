1ed919842f5c197057a78d5edba8fa263abc5ba7
dex.papa@gmail.com <dex.papa@gmail.com>
2017-11-25 23:44:52 +0530

    fixed sql query
    fixed sql query

--- btech.sql
+++ btech.sql
@@ -1,16 +1,16 @@
-Create Table educational_details_be
+create Table educational_details_be
 	(edu_id int primary key not null,
 	std_id 	int references student_data id not null,
 	ssc_school	varchar() not null,
 	ssc_year	varchar() not null,
 	ssc_percentage	varchar() not null,
 	ssc_class 	varchar() not null,
-	hsc_school	varcher() not null,
-	hsc_year	varcher() not null,
-	hsc_pcm		varcher() not null,
-	hsc_percentage	varcher() not null,
-	hsc_class	varcher() not null,
-	roll_no		varcher() not null,
+	hsc_school	varchar() not null,
+	hsc_year	varchar() not null,
+	hsc_pcm		varchar() not null,
+	hsc_percentage	varchar() not null,
+	hsc_class	varchar() not null,
+	roll_no		varchar() not null,
 	physics		int not null,
 	chemistry	int not null,
 	maths		int not null,
@@ -21,7 +21,7 @@ Create Table educational_details_be
 	acpc_no		varchar() not null,
 	acpc_merit	varchar() not null,
 	p1		varchar() not null,
-`	p2		varchar() not null,
+	p2		varchar() not null,
 	p3		varchar() not null,
 	p4		varchar() not null
 	);
