6aa6449c0edc99d01b4587907e275bf266b6359c
dex.papa@gmail.com <dex.papa@gmail.com>
2017-11-26 00:31:44 +0530

    Map Location
    Added Map on Contact us page
    Added Custom css for Map

--- contact-us.php
+++ contact-us.php
@@ -7,6 +7,10 @@
 			</div>
 		<div class="panel-body">
 			<!-- Map to College-->
+			<div class="map-box">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.609852422716!2d73.11997021433062!3d22.292764148803233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc877db07754d%3A0xae632f0b3fbb9f30!2sNavrachana+University!5e0!3m2!1sen!2sin!4v1511630534194" width="100%" height="372" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+			</div>
 
 		</div>
 	</div>
