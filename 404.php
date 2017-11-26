7ebbc7880928796b5962c550af7da84497bc1ae5
dex.papa@gmail.com <dex.papa@gmail.com>
2017-11-26 21:26:41 +0530

    Updated 404
    Updated Design of 404 page looks good now

--- 404.php
+++ 404.php
@@ -1,15 +1,16 @@
-<head>
-<title> Page not Found </title>
-</head>
-       <body>
-       	<div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading main-color-bg">
-                    <h3 class="panel-title"><center>404 Page Not Found<center></h3>
-                </div>
-                <div class="panel-body" style="margin-top:0px;">
-                        <center> The page you're looking for does not exist or you may have made a typo. Click <a href="/index.php">here</a> to go home page </center>
-                </div>
-            </div>
-          </div>
-     </body>
+<style type="text/css">
+      .center {text-align: center; margin-left: auto; margin-right: auto; margin-bottom: auto; margin-top: auto;}    
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<div class="container">
+  <div class="row">
+    <div class="span12">
+      <div class="hero-unit center">
+          <h1 style="margin-top:12%"><font face="Tahoma" color="red">Error 404</font></h1>
+          <br />
+          <p>The page you requested could not be found, either contact your webmaster or try again. Use your browsers <b>Back</b> button to navigate to the page you have prevously come from</p>
+          <p><b>Or you could just press this neat little button:</b></p>
+          <a href="../Admission/" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take Me Home</a>
+        </div>
+        <br />
+      
